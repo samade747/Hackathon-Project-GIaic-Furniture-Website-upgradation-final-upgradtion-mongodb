96d6f4f45f8e46f05b48a16eeb2878e003530aba
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-01-25 10:37:30 +0500

    1037

--- updatelatest/global.d.ts
+++
@@ -1,8 +0,0 @@
-// global.d.ts
-export {}
-
-declare global {
-  // augmenting global object
-  // We allow globalThis._mongoose to be any or a Promise
-  var _mongoose: Promise<typeof import("mongoose")> | undefined
-}
--- updatelatest/src/sanity/schemaTypes/index.ts
+++
@@ -1,9 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import product from "../schemas/product";
-import blog from "../schemas/blog";
-import category from '../schemas/category';
-import order from '../schemas/order';
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [product, blog, category, order],
-}

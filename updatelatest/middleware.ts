151d54236fcd1b697f1cf2ac4e8981a862f13d69
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-01-25 05:15:53 +0500

    Update middleware.ts

--- updatelatest/middleware.ts
+++ updatelatest/middleware.ts
@@ -1,22 +1,33 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// Define the routes that require authentication
-const isProtectedRoute = createRouteMatcher(['/shop(.*)', '/cart(.*)', '/checkout(.*)']);
-
-export default clerkMiddleware(async (auth, req) => {
-  const { userId, redirectToSignIn } = await auth();
-
-  // If the user is not authenticated and tries to access a protected route, redirect to sign-in
-  if (!userId && isProtectedRoute(req)) {
-    return redirectToSignIn({ returnBackUrl: req.url });
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Apply middleware to all routes
+    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
+
+
+
+// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+// const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
+
+// export default clerkMiddleware(async (auth, request) => {
+//   if (!isPublicRoute(request)) {
+//     await auth.protect()
+//   }
+// })
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Always run for API routes
+//     '/(api|trpc)(.*)',
+//   ],
+// }
